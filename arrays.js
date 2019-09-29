29794d653453502a7699a00f81e63bf1f13ffa46
patrick hoehn <patrh_99@hotmail.com>
2019-09-29 01:51:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(`${element}`);
   return array
 }
+
+function addElementToEndOfArray(array,element){
+  var anotherArray = [...array, `${element}`]
+}
